13b674ea523985d26622caf341911b5b6efb0a19
SeaumSiddiqui <seaumsiddiqui@yahoo.com>
2026-01-04 12:41:07 +0600

    chekcing for bug on blog section

--- personalblog-ui/src/components/portfolio/BlogSection.tsx
+++ personalblog-ui/src/components/portfolio/BlogSection.tsx
@@ -35,13 +35,13 @@ export const BlogSection: React.FC<BlogSectionProps> = ({ isDarkMode }) => {
 
   return (
     <section id="blogs" className="mb-16 scroll-mt-16 md:mb-24 lg:mb-36 lg:scroll-mt-24">
-      <div className="sticky top-0 z-20 -mx-6 mb-4 w-screen bg-opacity-75 px-6 py-5 backdrop-blur md:-mx-12 md:px-12 lg:sr-only lg:relative lg:top-auto lg:mx-auto lg:w-full lg:px-0 lg:py-0 lg:opacity-0">
+      {/* <div className="sticky top-0 z-20 -mx-6 mb-4 w-screen bg-opacity-75 px-6 py-5 backdrop-blur md:-mx-12 md:px-12 lg:sr-only lg:relative lg:top-auto lg:mx-auto lg:w-full lg:px-0 lg:py-0 lg:opacity-0">
         <h2 className={`text-sm font-bold uppercase tracking-widest ${
           isDarkMode ? 'text-dark-text' : 'text-light-text'
         }`}>
           Blog
         </h2>
-      </div>
+      </div> */}
 
       <div>
         <div className="space-y-6">
